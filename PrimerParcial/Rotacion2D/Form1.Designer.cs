99b6502e9f7e7bb969400f889ffc7a64f85ace4a
Marco Manzo <marcomanzoruiz@gmail.com>
2023-01-21 18:53:21 -0600

    Los 3 botones de Rotacion2D ya funcionan

--- PrimerParcial/Rotacion2D/Form1.Designer.cs
+++ PrimerParcial/Rotacion2D/Form1.Designer.cs
@@ -43,18 +43,20 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackColor = System.Drawing.Color.Black;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(36, 75);
+            this.pictureBox1.Location = new System.Drawing.Point(27, 61);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1272, 400);
+            this.pictureBox1.Size = new System.Drawing.Size(1426, 506);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 12);
+            this.button1.Location = new System.Drawing.Point(27, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(435, 29);
+            this.button1.Size = new System.Drawing.Size(326, 24);
             this.button1.TabIndex = 3;
             this.button1.Text = "Rotación teniendo como pivote el origen 0,0 en el centro de la pantalla";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,17 +64,19 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(36, 47);
+            this.textBox1.Location = new System.Drawing.Point(27, 38);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1272, 22);
+            this.textBox1.Size = new System.Drawing.Size(955, 20);
             this.textBox1.TabIndex = 4;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(477, 12);
+            this.button2.Location = new System.Drawing.Point(358, 10);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(447, 29);
+            this.button2.Size = new System.Drawing.Size(335, 24);
             this.button2.TabIndex = 5;
             this.button2.Text = "Rotar la figura colocando su centro en el origen en el centro de la pantlla\r\n";
             this.button2.UseVisualStyleBackColor = true;
@@ -80,9 +84,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(931, 12);
+            this.button3.Location = new System.Drawing.Point(698, 10);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(377, 29);
+            this.button3.Size = new System.Drawing.Size(283, 24);
             this.button3.TabIndex = 6;
             this.button3.Text = "Rotación de acuerdo al pivote de la figura en su centro\r\n";
             this.button3.UseVisualStyleBackColor = true;
@@ -90,14 +95,15 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1346, 526);
+            this.ClientSize = new System.Drawing.Size(1481, 607);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
