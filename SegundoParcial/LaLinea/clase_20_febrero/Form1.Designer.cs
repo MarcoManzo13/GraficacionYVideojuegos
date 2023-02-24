0a2b2ae3ce27ca6a8a1fede1123551d39b8ea1f9
Marco Manzo <marcomanzoruiz@gmail.com>
2023-02-24 12:41:07 -0600

    Finished Particle System

---
+++ SegundoParcial/LaLinea/clase_20_febrero/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace clase_20_febrero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PCT_CANVAS = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PCT_CANVAS
+            // 
+            this.PCT_CANVAS.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PCT_CANVAS.Location = new System.Drawing.Point(31, 60);
+            this.PCT_CANVAS.Name = "PCT_CANVAS";
+            this.PCT_CANVAS.Size = new System.Drawing.Size(776, 426);
+            this.PCT_CANVAS.TabIndex = 0;
+            this.PCT_CANVAS.TabStop = false;
+            this.PCT_CANVAS.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PCT_CANVAS_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 510);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PCT_CANVAS);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PCT_CANVAS;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SegundoParcial/SistemaDeParticulas1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeParticulas1
+{
+    public partial class Form1 : Form
+    {
+        private const int PARTICLE_COUNT = 100;
+        private const int PARTICLE_SIZE_MIN = 5;
+        private const int PARTICLE_SIZE_MAX = 15;
+        private const int PARTICLE_VELOCITY_MIN = -5;
+        private const int PARTICLE_VELOCITY_MAX = 5;
+        private const int PARTICLE_GENERATION_DELAY = 100;
+        private const int EMITTER_X = 600;
+        private const int EMITTER_Y = 200;
+        private const int MIN_SIZE = 10;
+        private const int MAX_SIZE = 25;
+        private const int MIN_SPEED = -5;
+        private const int MAX_SPEED = 30;
+        private const int GRAVITY = -1;
+        private const int NEW_PARTICLE_COUNT = 10;
+
+        private Particle[] particles;
+        private Random random;
+        private Timer timer;
+
+        Image ImagenDeFondo = Image.FromFile("C:\\Users\\marco\\OneDrive\\Documentos\\Udlap\\5to Semestre\\Graficación y Videojuegos\\SegundoParcial\\SistemaDeParticulas1\\Genkidama.jpg");
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeParticleSystem();
+            InitializeTimer();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void InitializeParticleSystem()
+        {
+            particles = new Particle[PARTICLE_COUNT];
+            random = new Random();
+
+            for (int i = 0; i < PARTICLE_COUNT; i++)
+            {
+                particles[i] = new Particle
+                {
+                    X = pictureBox1.Width / 2,
+                    Y = pictureBox1.Height / 2,
+                    Size = random.Next(PARTICLE_SIZE_MIN, PARTICLE_SIZE_MAX),
+                    Color = Color.FromArgb(50, 50, random.Next(150, 256)),
+                    VelocityX = random.Next(PARTICLE_VELOCITY_MIN, PARTICLE_VELOCITY_MAX),
+                    VelocityY = random.Next(PARTICLE_VELOCITY_MIN, PARTICLE_VELOCITY_MAX)
+                };
+            }
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = PARTICLE_GENERATION_DELAY;
+            timer.Tick += timer1_Tick;
+            timer.Start();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            // Dibujar las partículas
+            for (int i = 0; i < PARTICLE_COUNT; i++)
+                {
+                    Particle particle = particles[i];
+                    Brush brush = new SolidBrush(particle.Color);
+                    g.FillEllipse(brush, particle.X - particle.Size / 2, particle.Y - particle.Size / 2, particle.Size, particle.Size);
+                }
+            }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = ImagenDeFondo;
+            // Generar nuevas partículas
+            for (int i = 0; i < NEW_PARTICLE_COUNT; i++)
+            {
+                GenerateParticle();
+            }
+
+            // Actualizar posición de las partículas
+            for (int i = 0; i < PARTICLE_COUNT; i++)
+            {
+                particles[i].X += particles[i].VelocityX;
+                particles[i].Y += particles[i].VelocityY;
+                particles[i].VelocityY += GRAVITY;
+
+                // Verificar límites
+                if (particles[i].X < 0)
+                {
+                    particles[i].VelocityX *= -1;
+                    particles[i].X = 0;
+                }
+                else if (particles[i].X > pictureBox1.Width - particles[i].Size)
+                {
+                    particles[i].VelocityX *= -1;
+                    particles[i].X = pictureBox1.Width - particles[i].Size;
+                }
+
+                if (particles[i].Y < 0)
+                {
+                    particles[i].VelocityY *= -1;
+                    particles[i].Y = 0;
+                }
+                else if (particles[i].Y > pictureBox1.Height - particles[i].Size)
+                {
+                    particles[i].VelocityY *= -1;
+                    particles[i].Y = pictureBox1.Height - particles[i].Size;
+                }
+            }
+
+            // Redibujar las partículas
+            pictureBox1.Invalidate();
+        }
+
+        private void GenerateParticle()
+        {
+            Random random = this.random;
+
+            // Generate new particle
+            Particle particle = new Particle();
+            particle.X = EMITTER_X;
+            particle.Y = EMITTER_Y;
+            particle.Size = random.Next(MIN_SIZE, MAX_SIZE + 1);
+            particle.Color = Color.FromArgb(50, 50, random.Next(150, 256));
+            double speed = random.Next(MIN_SPEED, MAX_SPEED + 1);
+            double angle = random.NextDouble() * 2 * Math.PI;
+            particle.VelocityX = (float)(speed * Math.Cos(angle));
+            particle.VelocityY = (float)(speed * Math.Sin(angle));
+
+            // Replace a random particle with the new particle
+            particles[random.Next(PARTICLE_COUNT)] = particle;
+        }
+
+        public class Particle
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float VelocityX { get; set; }
+            public float VelocityY { get; set; }
+            public Color Color { get; set; }
+            public int Size { get; set; }
+        }
+    }
+}
